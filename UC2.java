9dc67984565ea32a60530b57a7dc4607d2855dcb
harshilbasketball <harshilbasketball0531@gmail.com>
2026-03-12 15:36:14 +0530

    Create UC2.java

---
+++ UC2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UC2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name:");
+        String name = scanner.next();
+
+        System.out.println("Hello " + name);
+    }
+}
